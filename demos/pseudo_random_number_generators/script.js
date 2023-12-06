759a4643b7ea959c744e9f7577e1bae1b8a0009c
Dedede <keeperofwonderland@gmail.com>
2023-12-06 21:43:29 +0700

    main - update

---
+++ demos/pseudo_random_number_generators/script.js
@@ -0,0 +1,10 @@
+const canvas = document.getElementById('canvas'),
+      context = canvas.getContext('2d'),
+      width = canvas.width = 600,
+      height = canvas.height = 600;
+
+function draw() {
+    middleSquareMethod();
+}
+
+draw();
