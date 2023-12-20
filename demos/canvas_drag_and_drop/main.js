3ec534e3c26e769e8c17dce481ad424679e06eaa
Dedede <keeperofwonderland@gmail.com>
2023-12-20 16:04:55 +0700

    main - canvas drag and drop init
    main - canvas drag and drop init

---
+++ demos/canvas_drag_and_drop/main.js
@@ -0,0 +1,58 @@
+const canvas = document.getElementById("canvas");
+const context = canvas.getContext("2d");
+const width = canvas.width = window.innerWidth;
+const height = canvas.height = window.innerHeight;
+
+let handle = {
+    x: width / 2,
+    y: height / 2,
+    radius: 20
+};
+
+let offset = {};
+
+function onMouseMove(event) {
+    handle.x = event.clientX;
+    handle.y = event.clientY;
+    draw();
+}
+
+function onMouseUp(event) {
+    document.body.removeEventListener('mousemove', onMouseMove);
+    document.body.removeEventListener('mouseup', onMouseUp);
+}
+
+function onMouseDown(event) {
+    if (
+        utils.circlePointCollision(
+            event.clientX,
+            event.clientY,
+            handle
+        )
+    ) {
+        document.body.addEventListener('mousemove', onMouseMove);
+        document.body.addEventListener('mouseup', onMouseUp);
+        offset.x = event.clientX - handle.x;
+        offset.y = event.clientY - handle.y;
+    }
+}
+
+function draw() {
+    context.clearRect(0, 0, width, height);
+
+    context.fillStyle = 'gray';
+    context.beginPath();
+    context.arc(
+        handle.x,
+        handle.y,
+        handle.radius,
+        0,
+        Math.PI * 2,
+        false
+    );
+    context.fill();
+}
+
+document.body.addEventListener('mousedown', onMouseDown);
+
+draw();
