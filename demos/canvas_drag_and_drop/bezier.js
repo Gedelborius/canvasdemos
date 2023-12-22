4e8744ac3768270cab071446bc63db66bde4ecec
Dedede <keeperofwonderland@gmail.com>
2023-12-22 10:49:34 +0700

    main - bezier update
    main - bezier update

--- demos/canvas_drag_and_drop/bezier.js
+++ demos/canvas_drag_and_drop/bezier.js
@@ -1,7 +1,7 @@
 const canvas = document.getElementById('canvas');
 const context = canvas.getContext('2d');
-const width = canvas.width = windows.innerWidth;
-const height = canvas.height = windows.innerHeight;
+const width = canvas.width = window.innerWidth;
+const height = canvas.height = window.innerHeight;
 
 let handle0 = {
     x: 100,
@@ -15,7 +15,7 @@ let handle1 = {
     radius: 15
 }
 
-let handle2= {
+let handle2 = {
     x: 700,
     y: 100,
     radius: 15
@@ -31,3 +31,80 @@ let handles = [handle0, handle1, handle2, handle3];
 let offset = {};
 let isDragging = false;
 let dragHandle = null;
+
+function onMouseMove(event) {
+    dragHandle.x = event.clientX - offset.x;
+    dragHandle.y = event.clientY - offset.y;
+}
+
+function onMouseUp(event) {
+    document.body.removeEventListener('mousemove', onMouseMove);
+    document.body.removeEventListener('mouseup', onMouseUp);
+    isDragging = false;
+}
+
+function draw() {
+    context.clearRect(0, 0, width, height);
+
+    context.beginPath();
+    context.moveTo(handle0.x, handle0.y);
+    context.bezierCurveTo(
+        handle1.x, handle1.y,
+        handle2.x, handle2.y,
+        handle3.x, handle3.y
+    );
+    context.stroke();
+
+    context.fillStyle = 'gray';
+
+    for (let i = 0; i < handles.length; i++) {
+        const handle = handles[i];
+
+        if (isDragging && handle === dragHandle) {
+            context.shadowColor = 'black';
+            context.shadowOffsetX = 4;
+            context.shadowOffsetY = 4;
+            context.shadowBlur = 8;
+        }
+
+        context.beginPath();
+        context.arc(
+            handle.x,
+            handle.y,
+            handle.radius,
+            0,
+            Math.PI * 2,
+            false
+        );
+        context.fill();
+
+        context.shadowColor = null;
+        context.shadowOffsetX = null;
+        context.shadowOffsetY = null;
+        context.shadowBlur = null;
+    }
+
+    requestAnimationFrame(draw)
+}
+
+document.body.addEventListener('mousedown', (event) => {
+    for (let i = 0; i < handles.length; i++) {
+        const handle = handles[i];
+        if (
+            utils.circlePointCollision(
+                event.clientX,
+                event.clientY,
+                handle
+            )
+        ) {
+            isDragging = true;
+            document.body.addEventListener('mousemove', onMouseMove);
+            document.body.addEventListener('mouseup', onMouseUp);
+            dragHandle = handle;
+            offset.x = event.clientX - handle.x;
+            offset.y = event.clientY - handle.y;
+        }
+    }
+})
+
+draw();
