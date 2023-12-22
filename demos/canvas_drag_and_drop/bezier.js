6fd8f14beab4ab37028f253e0d24b57edacb4235
Dedede <keeperofwonderland@gmail.com>
2023-12-22 21:03:05 +0700

    main - add quicksettings panel to bezier
    main - add quicksettings panel to bezier

--- demos/canvas_drag_and_drop/bezier.js
+++ demos/canvas_drag_and_drop/bezier.js
@@ -1,7 +1,72 @@
 const canvas = document.getElementById('canvas');
-const context = canvas.getContext('2d');
+const ctx = canvas.getContext('2d');
 const width = canvas.width = window.innerWidth;
 const height = canvas.height = window.innerHeight;
+
+const defaultParameters = {
+    backgroundColor: '#ffffff',
+    curveColor: '#000000',
+    handlesRadius: {
+        min: 1,
+        max: 100,
+        value: 15,
+        step: 1
+    },
+    handles: [
+        { x: 100, y: 100, r: 15, color: '#808080' },
+        { x: 400, y: 400, r: 15, color: '#808080' },
+        { x: 700, y: 100, r: 15, color: '#808080' },
+        { x: 1000, y: 500, r: 15, color: '#808080' }
+    ],
+    doRandomSpawn: false
+}
+
+const panelTitles = {
+    backgroundColor: 'Background color',
+    curveColor: 'Curve color',
+    handlesRadius: 'Point radius',
+    handlesColor: [
+        'Point 1 color',
+        'Point 2 color',
+        'Point 3 color',
+        'Point 4 color'
+    ],
+    doRandomSpawn: 'Do Random Spawn',
+    restart: 'Restart'
+}
+
+const panel = QuickSettings
+    .create(0, 0, 'Move me (Double click - hide)')
+    .addColor(panelTitles.backgroundColor, defaultParameters.backgroundColor)
+    .addColor(panelTitles.curveColor, defaultParameters.curveColor)
+    .addNumber(
+        panelTitles.handlesRadius,
+        defaultParameters.handlesRadius.min,
+        defaultParameters.handlesRadius.max,
+        defaultParameters.handlesRadius.value,
+        defaultParameters.handlesRadius.step
+    )
+    .addColor(panelTitles.handlesColor[0], defaultParameters.handles[0].color)
+    .addColor(panelTitles.handlesColor[1], defaultParameters.handles[1].color)
+    .addColor(panelTitles.handlesColor[2], defaultParameters.handles[2].color)
+    .addColor(panelTitles.handlesColor[3], defaultParameters.handles[3].color)
+    .addBoolean(panelTitles.doRandomSpawn, defaultParameters.doRandomSpawn)
+    .addButton(panelTitles.restart, start)
+
+
+const getPanelValue = {
+    backgroundColor: () => panel.getValue(panelTitles.backgroundColor),
+    curveColor: () => panel.getValue(panelTitles.curveColor),
+    handlesRadius: () => panel.getValue(panelTitles.handlesRadius),
+    handlesColor: [
+        () => panel.getValue(panelTitles.handlesColor[0]),
+        () => panel.getValue(panelTitles.handlesColor[1]),
+        () => panel.getValue(panelTitles.handlesColor[2]),
+        () => panel.getValue(panelTitles.handlesColor[3])
+    ],
+    doRandomSpawn: () => panel.getValue(panelTitles.doRandomSpawn)
+}
+
 
 let handles = [
     { x: 100, y: 100, r: 15 },
@@ -9,9 +74,33 @@ let handles = [
     { x: 700, y: 100, r: 15 },
     { x: 1000, y: 500, r: 15 }
 ];
+
+let updateRequest = null;
+let prng = null;
 let offset = {};
 let isDragging = false;
 let dragHandle = null;
+
+function initiateHandles(prng) {
+    for (let i = 0; i < 4; i++) {
+        let x = 0;
+        let y = 0;
+        if (getPanelValue.doRandomSpawn()) {
+            x = prng() * ctx.canvas.width;
+            y = prng() * ctx.canvas.height;
+        } else {
+            x = defaultParameters.handles[i].x;
+            y = defaultParameters.handles[i].y;
+        }
+        handles.push(
+            {
+                x,
+                y,
+                r: getPanelValue.handlesRadius(),
+            }
+        )
+    }
+}
 
 function onMouseMove(event) {
     dragHandle.x = event.clientX - offset.x;
@@ -24,48 +113,65 @@ function onMouseUp() {
     isDragging = false;
 }
 
-function draw() {
-    context.clearRect(0, 0, width, height);
+function drawBackground() {
+    ctx.fillStyle = getPanelValue.backgroundColor();
+    ctx.fillRect(0, 0, ctx.canvas.width, ctx.canvas.height);
+}
 
-    context.beginPath();
-    context.moveTo(handles[0].x, handles[0].y);
-    context.bezierCurveTo(
-        handles[1].x, handles[1].y,
-        handles[2].x, handles[2].y,
-        handles[3].x, handles[3].y
+function drawBezierCurve(points) {
+    ctx.strokeStyle = getPanelValue.curveColor();
+    ctx.beginPath();
+    ctx.moveTo(points[0].x, points[0].y);
+    ctx.bezierCurveTo(
+        points[1].x, points[1].y,
+        points[2].x, points[2].y,
+        points[3].x, points[3].y
     );
-    context.stroke();
+    ctx.stroke();
+}
 
-    context.fillStyle = 'gray';
+function drawCircle(point) {
+    ctx.beginPath();
+    ctx.arc(
+        point.x,
+        point.y,
+        point.r,
+        0,
+        Math.PI * 2,
+        false
+    );
+    ctx.fill();
+}
+
+function shadowOn() {
+    ctx.shadowColor = 'black';
+    ctx.shadowOffsetX = 4;
+    ctx.shadowOffsetY = 4;
+    ctx.shadowBlur = 8;
+}
+
+function shadowOff() {
+    ctx.shadowColor = null;
+    ctx.shadowOffsetX = null;
+    ctx.shadowOffsetY = null;
+    ctx.shadowBlur = null;
+}
+
+function udpate() {
+    drawBackground()
+    drawBezierCurve(handles);
 
     for (let i = 0; i < handles.length; i++) {
         const handle = handles[i];
-
+        ctx.fillStyle = getPanelValue.handlesColor[i]();
         if (isDragging && handle === dragHandle) {
-            context.shadowColor = 'black';
-            context.shadowOffsetX = 4;
-            context.shadowOffsetY = 4;
-            context.shadowBlur = 8;
+            shadowOn();
         }
-
-        context.beginPath();
-        context.arc(
-            handle.x,
-            handle.y,
-            handle.r,
-            0,
-            Math.PI * 2,
-            false
-        );
-        context.fill();
-
-        context.shadowColor = null;
-        context.shadowOffsetX = null;
-        context.shadowOffsetY = null;
-        context.shadowBlur = null;
+        drawCircle(handle);
+        shadowOff()
     }
 
-    requestAnimationFrame(draw)
+    updateRequest = requestAnimationFrame(udpate)
 }
 
 document.body.addEventListener('mousedown', (event) => {
@@ -88,4 +194,17 @@ document.body.addEventListener('mousedown', (event) => {
     }
 })
 
-draw();
+
+function start() {
+    if (updateRequest !== null) {
+        cancelAnimationFrame(updateRequest);
+    }
+
+    prng = new Math.seedrandom();
+    handles.length = 0;
+    initiateHandles(prng);
+
+    udpate();
+}
+
+start();
