cea8199b2047e57f3950c6b1aa3a8b63a26bb6f0
Dedede <keeperofwonderland@gmail.com>
2024-01-14 20:58:44 +0700

    main - add to aurora dat gui
    main - add to aurora dat gui

--- demos/aurora_animation_effect/scripts/main.js
+++ demos/aurora_animation_effect/scripts/main.js
@@ -7,7 +7,7 @@ const defaultParameters = {
         line: {
             array: null,
             parameters: {
-                count: null,
+                count: 0,
                 width: {
                     min: 10,
                     max: 30,
@@ -26,6 +26,30 @@ const defaultParameters = {
     }
 }
 
+function setGUI(cvs, scene) {
+    const gui = new dat.GUI();
+
+
+    const fBackgroundColor = gui.addFolder('Background color');
+    fBackgroundColor.addColor(scene.background, 'color');
+
+    const fLine = gui.addFolder('Line Settings');
+
+    const fLineWidth = fLine.addFolder('Line Width');
+    fLineWidth.add(scene.line.parameters.width, 'min', 1, 1000, 1).onChange(_ => resizeReset(cvs, scene));
+    fLineWidth.add(scene.line.parameters.width, 'max', 1, 1000, 1).onChange(_ => resizeReset(cvs, scene));
+
+    const fLineHeight = fLine.addFolder('Line Height');
+    fLineHeight.add(scene.line.parameters.height, 'min', 1, 1000, 1).onChange(_ => resizeReset(cvs, scene));
+    fLineHeight.add(scene.line.parameters.height, 'max', 1, 1000, 1).onChange(_ => resizeReset(cvs, scene));
+
+    const fLineTTL = fLine.addFolder('Line TTL');
+    fLineTTL.add(scene.line.parameters.ttl, 'min', 1, 1000, 1).onChange(_ => resizeReset(cvs, scene));
+    fLineTTL.add(scene.line.parameters.ttl, 'max', 1, 1000, 1).onChange(_ => resizeReset(cvs, scene));
+
+    return gui;
+}
+
 function start() {
     canvasHelper.create.container('canvas_container');
     canvasHelper.create.canvas('back');
@@ -34,13 +58,13 @@ function start() {
     canvasHelper.insertBeforeFirst.container();
     canvasHelper.insertBeforeFirst.canvas();
 
+
     let canvasObject = {
         canvas: canvasHelper.canvas(),
         renderingContext2D: canvasHelper.context()
     };
-    let sceneObject = {
-        ...defaultParameters.scene
-    }
+    let sceneObject = { ...defaultParameters.scene }
+    const gui = setGUI(canvasObject, sceneObject);
 
     resizeReset(canvasObject, sceneObject);
     animationLoop(canvasObject, sceneObject);
