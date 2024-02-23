38d03fb3cdf05776bfdc67728db28cc22cf24ef6
Sergey Tokarev <sergey.zoomaster@gmail.com>
2024-02-23 23:12:06 +0700

    main - scripts2 test

---
+++ demos/conway_game_of_life_20240123/scripts2/canvas.js
@@ -0,0 +1,7 @@
+function getCanvas(){
+    return document.getElementById('#canvas');
+}
+
+function getContext(){
+    return getCanvas()?.getContext('2d');
+}
---
+++ demos/conway_game_of_life_20240123/scripts2/index.js
@@ -0,0 +1,7 @@
+console.log('test index');
+
+const canvas = getCanvas();
+const context = getContext();
+
+console.log('canvas got: ', canvas!=null);
+console.log('context got: ', context!=null);
